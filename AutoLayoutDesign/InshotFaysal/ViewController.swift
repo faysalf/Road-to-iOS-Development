e555546424e73d4f68b27aab4a047e5c34bb2236
Md. Faysal Ahmed <82731243+faysalf@users.noreply.github.com>
2022-10-29 03:09:12 -0700

    Add files via upload

---
+++ AutoLayoutDesign/InshotFaysal/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  InshotFaysal
+//
+//  Created by Md. Faysal Ahmed on 28/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var view2Width: NSLayoutConstraint!
+    @IBOutlet weak var view2Height: NSLayoutConstraint!
+    @IBOutlet weak var bottomConst: NSLayoutConstraint!
+    
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var img1: UIImageView!
+    @IBOutlet weak var img2: UIImageView!
+    @IBOutlet weak var img3: UIImageView!
+    
+    @IBOutlet weak var circleImg1: UIImageView!
+    @IBOutlet weak var circleImg2: UIImageView!
+    @IBOutlet weak var circleImg3: UIImageView!
+    @IBOutlet weak var testStackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //cornerRadius()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        if (view.frame.width <= 380) {
+            view2Width.constant = 334;
+            view2Height.constant = 175
+        }
+    }
+    
+//    func cornerRadius() {
+//        view2.layer.cornerRadius = 15
+//        img1.layer.cornerRadius = 10
+//        img2.layer.cornerRadius = 10
+//        img3.layer.cornerRadius = 10
+//        circleImg1.layer.cornerRadius = 0.5 * circleImg1.frame.size.height
+//        circleImg2.layer.cornerRadius = 0.5 * circleImg2.frame.size.height
+//        circleImg3.layer.cornerRadius = 0.5 * circleImg3.frame.size.height
+//
+//    }
+
+
+}
+
