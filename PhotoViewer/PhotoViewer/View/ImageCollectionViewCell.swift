71536b1c38e46e7f26abefc60ea91072c10b6731
Md. Faysal Ahmed <82731243+faysalf@users.noreply.github.com>
2022-11-04 06:05:33 -0700

    Add files via upload

---
+++ PhotoViewer/PhotoViewer/View/ImageCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ImageCollectionViewCell.swift
+//  PhotoViewer
+//
+//  Created by Md. Faysal Ahmed on 3/11/22.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
